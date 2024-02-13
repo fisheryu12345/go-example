6d476609accaa7e8011934bce9c45060c65ab7c7
fisheryu12345 <136813813+fisheryu12345@users.noreply.github.com>
2024-02-13 10:01:59 +0800

    232321
    123123

---
+++ web/src/layout/footer/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="layout-footer pb5 pt2">
+		<div class="layout-footer-warp">
+			<div>Powered by HSBC China</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts" name="layoutFooter">
+// 此处需有内容（注释也得），否则缓存将失败
+</script>
+
+<style scoped lang="scss">
+.layout-footer {
+	width: 100%;
+	display: flex;
+	&-warp {
+    color: var(--el-text-color-secondary);
+		text-align: center;
+    margin: 30px auto auto;
+    animation: error-num 0.3s ease;
+	}
+}
+</style>
---
+++ web/src/views/demo/demo.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import {ElMessage} from "element-plus";
+import {DeleteFilled, Present} from "@element-plus/icons-vue";
+
+function query() {
+  ElMessage(
+      {
+        type: "success",
+        message: "query"
+      }
+  )
+
+}
+</script>
+
+<template>
+  <el-card class="box-card1" style="margin: 10px 0px 20px 10px">
+    <template #header>
+      <div class="card-header">
+        <span>Query Condition</span>
+        <!--        <el-button class="button" text>Operation button</el-button>-->
+      </div>
+    </template>
+    <!--    <div v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</div>-->
+    <div style="display: flex">
+      <el-input placeholder="请输入" style="margin-right: 10px"></el-input>
+      <el-input placeholder="请输入" style="margin-right: 10px"></el-input>
+      <el-input placeholder="请输入"></el-input>
+    </div>
+
+    <template #footer>
+      <div style="display: flex;justify-content: flex-end;">
+
+        <el-button type="primary" @click="query" round>
+          <el-icon class="el-icon--right">
+            <search/>
+          </el-icon>
+          Query
+        </el-button>
+        <el-button type="primary" round>
+          <el-icon class="el-icon--right">
+            <delete-filled/>
+          </el-icon>
+          Reset
+        </el-button>
+      </div>
+
+    </template>
+  </el-card>
+
+
+  <el-card class="box-card2">
+    <template #header>
+      <div class="card-header">
+        <span>Result</span>
+        <!--        <el-button class="button" text>Operation button</el-button>-->
+      </div>
+    </template>
+    <div v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</div>
+    <template #footer>Footer content</template>
+  </el-card>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
